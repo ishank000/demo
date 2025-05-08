1d06e3e5f0956b5b281b4f18027fa692d491bbc8
Ishank Sahni <143020424+ishank000@users.noreply.github.com>
2025-05-09 02:07:08 +0530

    Delete src directory

--- src/services/dashboard.js
+++
@@ -1,3 +0,0 @@
-import axios from '../utils/axios';
-
-export const getMonthlySales = () => axios.get('https://bc975c36-3b0f-4b75-9cf1-3a5381439847.mock.pstmn.io/data');
--- src/views/sample/index.jsx
+++
@@ -1,9 +0,0 @@
-import VerificationCard from '../../components/Card/VerficationCard.jsx';
-
-export default function Sample() {
-  return (
-    <div>
-      <VerificationCard value={500} max={1000} />
-    </div>
-  );
-}
